36115867d01caaa4e222b5d80a89ca5bac90fc8d
2Fabio3Toms4Naene <fabiotomasnaene@gmail.com>
2025-03-29 15:52:14 +0200

    i created a function to ask the user his choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,4 +3,10 @@ function getComputerCoice() { //GETING THE COMPUTER CHOICE
     let randomChoice = arrayChoices[Math.floor(Math.random() * arrayChoices.length)];
     return console.log(randomChoice);
 };
+function getHumanChoice() { //GETING THE HUMAN CHOICE
+    const humanChoice = prompt("Now, please enter your choice: Rock, Paper or Scissors?");
+    return console.log(humanChoice);
+}
+
+getComputerCoice();
 getComputerCoice();
