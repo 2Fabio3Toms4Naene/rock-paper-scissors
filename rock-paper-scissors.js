d6d9d08fa57cd6bc43ce7636bcb9d7ca5ec225b3
2Fabio3Toms4Naene <fabiotomasnaene@gmail.com>
2025-03-29 15:50:57 +0200

    i created a function to print the computer random choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,6 @@
-console.log("Rock, Paper, Scissors Game");
+function getComputerCoice() { //GETING THE COMPUTER CHOICE
+    const arrayChoices = ["Rock", "Paper", "Scissors"];
+    let randomChoice = arrayChoices[Math.floor(Math.random() * arrayChoices.length)];
+    return console.log(randomChoice);
+};
+getComputerCoice();
